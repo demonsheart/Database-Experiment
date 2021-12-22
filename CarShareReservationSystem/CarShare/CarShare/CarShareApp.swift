30253a33472907272b48689379cbc97e24a9ea34
demonsheart <a2509875617@gmail.com>
2021-12-22 15:17:36 +0800

    change to swiftui

---
+++ CarShareReservationSystem/CarShare/CarShare/CarShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarShareApp.swift
+//  CarShare
+//
+//  Created by herongjin on 2021/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CarShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
